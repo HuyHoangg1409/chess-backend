3a4a8c70cb6d30f831db6341ed6b23562b1766ae
HuyHoangg1409 <hoangdilamviec@gmail.com>
2026-07-16 16:20:00 +0700

    feat: Thêm file secure.py để tạo JWT

--- chess_backend/config.py
+++ chess_backend/config.py
@@ -3,8 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://postgres:password@localhost:5432/my_db"
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(env_file=".env")
 
 
-settings = Settings()
+settings = Settings() # type: ignore
