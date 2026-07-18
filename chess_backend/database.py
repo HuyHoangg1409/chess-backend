925027d2c8091a67338dbd1898910e0db59f7259
HuyHoangg1409 <hoangdilamviec@gmail.com>
2026-07-18 14:16:55 +0700

    refactor: Thay Column bằng Mapped và DeclarativeBase để có type hint và xóa lỗi Pylance

--- chess_backend/database.py
+++ chess_backend/database.py
@@ -7,8 +7,6 @@ engine = create_engine(settings.DATABASE_URL)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
 
 def get_db():
     db = sessionLocal()
--- chess_backend/models.py
+++ chess_backend/models.py
@@ -1,31 +1,41 @@
 from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey
-from database import Base
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, nullable=False, index=True)
-    password_hash = Column(String, nullable=False)
-    elo_rating = Column(Integer, default=1200)
+    user_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(
+        String, unique=True, nullable=False, index=True
+    )
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
+    elo_rating: Mapped[int] = mapped_column(Integer, default=1200)
 
 
 class Puzzles(Base):
     __tablename__ = "puzzles"
 
-    puzzle_id = Column(Integer, primary_key=True, index=True)
-    fen_position = Column(Text, nullable=False)
-    correct_moves = Column(Text, nullable=False)
-    difficulty = Column(String, nullable=False)
+    puzzle_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    fen_position: Mapped[str] = mapped_column(Text, nullable=False)
+    correct_moves: Mapped[str] = mapped_column(Text, nullable=False)
+    difficulty: Mapped[str] = mapped_column(String, nullable=False)
 
 
 class UserPuzzleHistory(Base):
     __tablename__ = "user_puzzle_history"
 
-    puzzle_history_id = Column(Integer, primary_key=True, index=True)
-
-    user_id = Column(Integer, ForeignKey("user.user_id"), nullable=False)
-    puzzle_id = Column(Integer, ForeignKey("puzzles.puzzle_id"), nullable=False)
-
-    is_correct = Column(Boolean, nullable=False)
+    puzzle_history_id: Mapped[str] = mapped_column(
+        Integer, primary_key=True, index=True
+    )
+    user_id: Mapped[str] = mapped_column(
+        Integer, ForeignKey("user.user_id"), nullable=False
+    )
+    puzzle_id: Mapped[str] = mapped_column(
+        Integer, ForeignKey("puzzles.puzzle_id"), nullable=False
+    )
+    is_correct: Mapped[str] = mapped_column(Boolean, nullable=False)
