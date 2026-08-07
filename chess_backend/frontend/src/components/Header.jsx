0a7023d6ab104331775a36a05eebc3f8a5760b08
HuyHoangg1409 <hoangdilamviec@gmail.com>
2026-08-07 15:42:25 +0700

    style(Header): Tạo khung giao diện cho thông tin người dùng và làm lại nút đăng xuất

--- chess_backend/frontend/src/components/Header.jsx
+++ chess_backend/frontend/src/components/Header.jsx
@@ -7,18 +7,19 @@ export default function Header({ currentUser, onLogout }) {
         <h2 className="text-4xl text-green-600 font-bold">CHESS</h2>
       </div>
 
-      <button
-        onClick={onLogout}
-        className="px-4 py-2 bg-rose-600 hover:bg-rose-500 text-white font-bold rounded-lg text-sm cursor-pointer"
-      >
-        Log Out
-      </button>
+      <div className="flex items-center gap-5">
+        <div className="position- flex items-center gap-3 bg-[#17171A] px-3 py-1.5 rounded-lg border border-chess-border">
+          <div className="flex justify-center items-center w-8 h-8 rounded-full bg-emerald-600 font-semibold text-white">
+            {currentUser.username?currentUser.username.charAt(0).toUpperCase():'U'}
+          </div>
 
-      <div className="flex items-baseline gap-2.5 ">
-        <span className="text-2xl font-semibold text-red-400">
-          {currentUser.username}
-        </span>
-        <span className="text-green-500">{currentUser.elo_rating} ELO</span>
+          <div className="flex flex-col text-left">
+            <span className="text-sm font-semibold text-white">{currentUser.username}</span>
+            <span className="text-xs font-mono">{currentUser.elo_rating} <span>ELO</span></span>
+          </div>
+        </div>
+
+        <button onClick={onLogout} className="flex items-center justify-center size-9 leading-none text-white bg-red-600 text-xl rounded-full border border-chess-border cursor-pointer hover:bg-red-700 transition-colors duration-300">⏻</button>
       </div>
     </header>
   );
